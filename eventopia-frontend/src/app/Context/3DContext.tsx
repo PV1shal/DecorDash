536216df3561ad9875ce364ed5ebf4c7e64cbcaf
Vishal Pugazhendhi <vishal.pugazhendhi@gmail.com>
2024-10-29 22:29:25 -0700

    Add context for properties

--- eventopia-frontend/src/app/Context/3DContext.tsx
+++ eventopia-frontend/src/app/Context/3DContext.tsx
@@ -1,58 +1,41 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-interface Position {
-  lat: number;
-  lng: number;
-  altitude: number;
-}
-
-interface Orientation {
-  heading: number;
-  tilt: number;
-  roll: number;
-}
-
-interface AssetProperties {
-  position: Position;
-  scale: number;
-  src: string;
-  orientation: Orientation;
-  altitudeMode: string;
-}
-
-interface AssetContextType {
-    assetList: AssetProperties[];
-    handleDrop: (assetProperties: Partial<AssetProperties>) => void;
-}
-
 const AssetContext = createContext<AssetContextType>({
-    assetList: [],
-    handleDrop: () => {},
+  assetList: [],
+  handleDrop: () => {},
 });
 
-export const AssetContextProvider = ({children}) => {
+export const AssetContextProvider = ({ children }) => {
   const [assetList, setAssetList] = useState<AssetProperties[]>([]);
 
   const handleDrop = (assetProperties: Partial<AssetProperties>) => {
     let newAsset: AssetProperties = {
-        position: {
-            lat: assetProperties.position?.lat ?? 0,
-            lng: assetProperties.position?.lng ?? 0,
-            altitude: assetProperties.position?.altitude ?? 0,
-        },
-        scale: assetProperties.scale ?? 1,
-        orientation: assetProperties.orientation ?? { heading: 0, tilt: 0, roll: 0 },
-        src: assetProperties.src ?? "",
-        altitudeMode: "RELATIVE_TO_GROUND",
+      position: {
+        lat: assetProperties.position?.lat ?? 0,
+        lng: assetProperties.position?.lng ?? 0,
+        altitude: assetProperties.position?.altitude ?? 0,
+      },
+      scale: assetProperties.scale ?? 1,
+      orientation: assetProperties.orientation ?? {
+        heading: 0,
+        tilt: 0,
+        roll: 0,
+      },
+      src: assetProperties.src ?? "",
+      altitudeMode: "RELATIVE_TO_GROUND",
     };
 
     console.log("Dropped asset", newAsset);
     setAssetList((prevList) => [...prevList, newAsset]);
   };
 
-  return <AssetContext.Provider value={{ assetList: assetList, handleDrop: handleDrop}}>
-    {children}
-  </AssetContext.Provider>
-}
+  return (
+    <AssetContext.Provider
+      value={{ assetList: assetList, handleDrop: handleDrop }}
+    >
+      {children}
+    </AssetContext.Provider>
+  );
+};
 
 export const useAssetContext = () => useContext(AssetContext);
--- eventopia-frontend/src/app/components/MapComponent/map.tsx
+++ eventopia-frontend/src/app/components/MapComponent/map.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef, useState } from "react";
 import { useDrop } from "react-dnd";
 import itemTypes from "../../../../utils/ItemTypes";
 import { useAssetContext } from "@/app/Context/3DContext";
+import { AssetPropertiesComponent } from "../AssetComponents/AssetPropertiesComponent";
 
 const MapComponent = () => {
   const mapRef = useRef(null);
@@ -122,78 +123,7 @@ const MapComponent = () => {
   return (
     <div className="relative w-full h-full">
       <div ref={dropRef(mapRef)} className="w-full h-full"></div>
-      <div className="absolute top-4 right-4 bg-[#2C2C2C] bg-opacity-80 p-4 rounded flex flex-col items-center shadow">
-        <h3 className="text-lg font-bold mb-2 text-white">Asset Properties</h3>
-        <div className="mb-2">
-          <label className="block text-white">Latitude:</label>
-          <input
-            type="number"
-            value={assetProperties.position.lat}
-            onChange={(e) => handleAssetChange(e, 'position', 'lat')}
-            className="w-full border rounded px-2 py-1 bg-black text-white border-none"
-          />
-        </div>
-        <div className="mb-2">
-          <label className="block text-white">Longitude:</label>
-          <input
-            type="number"
-            value={assetProperties.position.lng}
-            onChange={(e) => handleAssetChange(e, 'position', 'lng')}
-            className="w-full border rounded px-2 py-1 bg-black text-white border-none"
-          />
-        </div>
-        <div className="mb-2">
-          <label className="block text-white">Altitude:</label>
-          <input
-            type="number"
-            value={assetProperties.position.altitude}
-            onChange={(e) => handleAssetChange(e, 'position', 'altitude')}
-            className="w-full border rounded px-2 py-1 bg-black text-white border-none"
-          />
-        </div>
-        <div className="mb-2">
-          <label className="block text-white">Scale:</label>
-          <input
-            type="number"
-            value={assetProperties.scale}
-            onChange={(e) => handleAssetChange(e, 'scale')}
-            className="w-full border rounded px-2 py-1 bg-black text-white border-none"
-          />
-        </div>
-        <div className="mb-2">
-          <label className="block text-white">Heading:</label>
-          <input
-            type="number"
-            value={assetProperties.orientation.heading}
-            onChange={(e) => handleAssetChange(e, 'orientation', 'heading')}
-            className="w-full border rounded px-2 py-1 bg-black text-white border-none"
-          />
-        </div>
-        <div className="mb-2">
-          <label className="block text-white">Tilt:</label>
-          <input
-            type="number"
-            value={assetProperties.orientation.tilt}
-            onChange={(e) => handleAssetChange(e, 'orientation', 'tilt')}
-            className="w-full border rounded px-2 py-1 bg-black text-white border-none"
-          />
-        </div>
-        <div className="mb-2">
-          <label className="block text-white">Roll:</label>
-          <input
-            type="number"
-            value={assetProperties.orientation.roll}
-            onChange={(e) => handleAssetChange(e, 'orientation', 'roll')}
-            className="w-full border rounded px-2 py-1 bg-black text-white border-none"
-          />
-        </div>
-        <button
-          onClick={handleDropAtCoordinates}
-          className="bg-[#DF2F67] text-white mt-3 px-4 py-2 rounded hover:bg-blue-600"
-        >
-          Drop Asset Here
-        </button>
-      </div>
+      <AssetPropertiesComponent />
     </div>
   );
 };
